0557c4a36cb510e4fab90a4b8eb9c9bd7b03014f
kelechukwufavour <kelechukwufavour.faye@gmail.com>
2023-10-13 14:59:17 -0700

    task 4

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,17 @@ import uuid
 from datetime import datetime
 
 class BaseModel():
-    def __init__(self):
+    def __init__(self, *args **kwargs):
+
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue;
+                elif key == "created_at" or key == "updated_at":
+                    self.__dict__[key] = datetime.fromisoformat(value);
+                else:
+                    self.__dict__[key] = value;
+    else:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.created_at = datetime.now()
