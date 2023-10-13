8ab41aa95f414d664b7e6d200145be413e49260d
Ugo <alexarinzeugo@yahoo.com>
2023-10-13 23:02:34 +0000

    added doc

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,18 @@
 import uuid
 from datetime import datetime
 
-class BaseModel():
-    def __init__(self, *args, **kwargs):
+'''
+Main parent class to all classes in the Airbnb clone project
+'''
 
+class BaseModel():
+    '''
+    Parent class to all classes in the Airbnb clone project
+    '''
+    def __init__(self, *args, **kwargs):
+        '''
+        Initialise attributes: uuid4, dates when class was created/updated
+        '''
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -19,12 +28,21 @@ class BaseModel():
             self.created_at = datetime.now()
 
     def __str__(self):
+        '''
+        Return class name, id, and the dictionary
+        '''
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        '''
+        Instance method to update current datetime, invoke save and save to serializes file
+        '''
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        '''
+        Return dictionary of basemodel with string formats of times
+        '''
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = self.created_at.isoformat()
