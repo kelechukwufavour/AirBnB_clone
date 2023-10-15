f052996c9d4737ceb03d1594e24f564b7889744f
Ugo <alexarinzeugo@yahoo.com>
2023-10-15 07:02:58 +0000

    made files pycodestyle compliant

--- models/user.py
+++ models/user.py
@@ -6,14 +6,14 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Represents a User.
-    
+
     Attributes:
         email (str): The email of the user.
         password (str): The password of the user.
         first_name (str): The first nane of the user.
         last_name (str): The last name if the user.
     """
-    
+
     email = ""
     password = ""
     first_name = ""
